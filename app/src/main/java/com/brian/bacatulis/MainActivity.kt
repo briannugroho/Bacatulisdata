34222fddffc56c4a3e8980315d4e8b6fe30f38f9
Brian wahyu nugroho <99382089+briannugroho@users.noreply.github.com>
2022-06-09 15:09:23 +0700

    brian wahyu nugroho

--- app/src/main/java/com/brian/bacatulis/MainActivity.kt
+++ app/src/main/java/com/brian/bacatulis/MainActivity.kt
@@ -4,10 +4,20 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_ACCELEROMETER
+import android.hardware.Sensor.TYPE_MAGNETIC_FIELD
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_GAME
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.view.animation.Animation.RELATIVE_TO_SELF
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,30 +27,41 @@ import com.brian.bacatulis.databinding.ActivityMainBinding
 import com.brian.bacatulis.model.InternalFileRepository
 import com.brian.bacatulis.model.Note
 import com.brian.bacatulis.model.NoteRepository
+import java.lang.Math.toDegrees
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity() , LocationListener {
-
+class MainActivity : AppCompatActivity(), SensorEventListener,LocationListener {
+    lateinit var sensorManager: SensorManager
+    lateinit var image: ImageView
+    lateinit var accelerometer: Sensor
+    lateinit var magnetometer: Sensor
     private val repo: NoteRepository by lazy { InternalFileRepository(this) }
     private lateinit var binding: ActivityMainBinding
     private lateinit var locationManager: LocationManager
     private lateinit var gpbtngetgps: TextView
     private val PermissionCode = 2
-
+    var currentDegree = 0.0f
+    var lastAccelerometer = FloatArray(3)
+    var lastMagnetometer = FloatArray(3)
+    var lastAccelerometerSet = false
+    var lastMagnetometerSet = false
+//membuat objek binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        image = findViewById(R.id.imageViewCompass) as ImageView
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD)
 
-
-
-
+//tombol untuk mencari lokasi
         binding.btngetgps.setOnClickListener {
             getLocation()
         }
-
+//memasukan data gps
         binding.logtofile.setOnClickListener {
             var logDataSensor = binding.editTeksCatatan.text.toString()
             val timeStamp: String = SimpleDateFormat("yy-MM-dd").format(Date())
@@ -50,6 +71,7 @@ class MainActivity : AppCompatActivity() , LocationListener {
             logDataSensor = "$logDataSensor , $logData1\n"
             binding.editTeksCatatan.setText(logDataSensor)
         }
+    //fungsi tombol tulis untuk menyimpan file
         binding.btnWrite.setOnClickListener {
             if (binding.editFileName.text.isNotEmpty()) {
                 try {
@@ -69,7 +91,7 @@ class MainActivity : AppCompatActivity() , LocationListener {
                 Toast.makeText(this, "Please provide a Filename", Toast.LENGTH_LONG).show()
             }
         }
-
+//fungsi untuk membuka file
         binding.btnRead.setOnClickListener {
             if (binding.editFileName.text.isNotEmpty()) {
                 try {
@@ -83,7 +105,7 @@ class MainActivity : AppCompatActivity() , LocationListener {
                 Toast.makeText(this, "Please provide a Filename", Toast.LENGTH_LONG).show()
             }
         }
-
+//fungsi menghapus file
         binding.btnDelete.setOnClickListener {
             if (binding.editFileName.text.isNotEmpty()) {
                 try {
@@ -101,6 +123,16 @@ class MainActivity : AppCompatActivity() , LocationListener {
             } else {
                 Toast.makeText(this, "Please provide a Filename", Toast.LENGTH_LONG).show()
             }
+        }
+    //untuk membagikan alamat lokasi
+        binding.btnbagi.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            val logData1 = binding.textView.text.toString()
+            intent.putExtra(Intent.EXTRA_TEXT, logData1)
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here")
+            val chooser = Intent.createChooser(intent, "Bagikan Dengan : ")
+            startActivity(chooser)
         }
     }
     private fun getLocation() {
@@ -134,6 +166,58 @@ class MainActivity : AppCompatActivity() , LocationListener {
             } else {
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager.registerListener(this, accelerometer, SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetometer, SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this, accelerometer)
+        sensorManager.unregisterListener(this, magnetometer)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor === accelerometer) {
+            lowPass(event.values, lastAccelerometer)
+            lastAccelerometerSet = true
+        } else if (event.sensor === magnetometer) {
+            lowPass(event.values, lastMagnetometer)
+            lastMagnetometerSet = true
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            val r = FloatArray(9)
+            if (SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(r, orientation)
+                val degree = (toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
+
+                val rotateAnimation = RotateAnimation(
+                    currentDegree,
+                    -degree,
+                    RELATIVE_TO_SELF, 0.5f,
+                    RELATIVE_TO_SELF, 0.5f)
+                rotateAnimation.duration = 1000
+                rotateAnimation.fillAfter = true
+
+                image.startAnimation(rotateAnimation)
+                currentDegree = -degree
+            }
+        }
+    }
+
+    fun lowPass(input: FloatArray, output: FloatArray) {
+        val alpha = 0.05f
+
+        for (i in input.indices) {
+            output[i] = output[i] + alpha * (input[i] - output[i])
         }
     }
 }
